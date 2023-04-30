00479a8ea2e53e115f868db125b655d07ecd4459
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-04-30 18:26:51 -0400

    index lab done

--- index.js
+++ index.js
@@ -1,5 +1,79 @@
 /* Your Code Here */
+function createEmployeeRecord (array) {
+    const testEmployee= {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return testEmployee
+}
+ 
+function createEmployeeRecords (array) {
+ return array.map(createEmployeeRecord)
 
+}
+
+function createTimeInEvent (dateStamp) {
+   //When timestamps are needed, they will be provided as Strings in the form: 
+   //"YYYY-MM-DD 800" or "YYYY-MM-DD 1800" e.g. "2018-01-01 2300"
+
+   const [date, hour] =dateStamp.split (' ')
+    const timeIn = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+
+  this.timeInEvents.push(timeIn)
+    return this
+}
+
+function createTimeOutEvent (dateStamp) {
+    const [date, hour] =dateStamp.split (' ')
+    const timeOut = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    let hoursWorked = 0
+    for (let i=0; i < this.timeInEvents.length; i++){
+      if(this.timeInEvents[i].date === date){
+         hoursWorked = (this.timeOutEvents[i].hour - this.timeInEvents[i].hour) * .01
+       break  
+      }  
+  }
+  return hoursWorked
+}
+
+function wagesEarnedOnDate (date) {
+    let payHour = 0
+    let hoursWorked=0
+    hoursWorked=hoursWorkedOnDate.call(this, date)
+    payHour = (payHour + hoursWorked) * this.payPerHour
+  
+    return payHour
+}
+
+function  findEmployeeByFirstName (empArray, firstName) {
+    return empArray.find (employee=>employee.firstName===firstName)
+}
+
+function  calculatePayroll (array){
+    let payRoll=0
+    for (let i = 0; i < array.length; i++) {
+        payRoll = payRoll + allWagesFor.call(array[i])
+    }   
+   return payRoll
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
